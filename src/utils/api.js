ff30848df51fd3211b3ad62fff916cc9cc2f1c8a
HumbertoIsraelLV <63939946+HumbertoIsraelLV@users.noreply.github.com>
2021-11-28 20:34:40 -0600

    change of server url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const serverApi = "http://localhost:5002/api";
-// const serverApi = "https://meet-app-backend.herokuapp.com/api";
+// const serverApi = "http://localhost:5002/api";
+const serverApi = "https://meet-app-backend.herokuapp.com/api";
 
 export const getRoomExists = async (roomId) => {
     const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
